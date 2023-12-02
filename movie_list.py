73a132e58e4c2ad7b93bb00730f2361b60694776
waiphyo <waiphyo@unc.edu>
2023-12-01 20:26:24 -0500

    .

---
+++ movie_list.py
@@ -0,0 +1,4 @@
+list[str] = ["The Shawshank Redemption", "The Godfather", "Titanic", "Star Wars: Episode IV - A New Hope", "Jurassic Park", "Avatar", 
+             "Harry Potter and the Sorcerer's Stone", "Forrest Gump", "Inception", "The Matrix", "Pulp Fiction", 
+             "The Lord of the Rings: The Fellowship of the Ring", "Fight Club", "The Dark Knight", "Schindler's List", "Toy Story", "Saving Private Ryan",
+             "Amélie", "The Lion King", "Gladiator", "Parasite", "La La Land", "The Silence of the Lambs", "Interstellar", "Spirited Away"]
